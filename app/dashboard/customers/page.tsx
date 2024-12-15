e82905f371f7caafd6b0355bb775df8542bd8d3b
wahaj67 <160678510+wahaj67@users.noreply.github.com>
2024-12-15 18:35:18 +0500

    new chages

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,6 @@
+export default function Customers() {
+    return (
+        <p>Customers Page</p>
+    )
+}
+
---
+++ app/ui/font.ts
@@ -0,0 +1,11 @@
+import {Inter,Lusitana} from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin'],
+})
+
+export const lustiana = Lusitana({
+    subsets:['latin'],
+    weight:['400','700']
+
+})
